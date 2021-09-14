2d9ff0e0af1e167bbc17eb640e594cd2432d02c6
lara <lara@vehicle4change.org>
2021-09-14 18:04:35 -0400

    router

--- src/Router.js
+++ src/Router.js
@@ -1,31 +1,29 @@
-import React from "react"
-import { Switch, Route } from "react-router-dom"
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 // home page
 // portfolio sections
-import About from "./components/pages/About.js";
-import Skills from "./components/pages/Skills.js";
-import Experience from "./components/pages/Experience.js";
-import Education from "./components/pages/Education.js";
-import Portfolio from "./components/pages/Portfolio.js";
-import Contact from "./components/pages/Contact.js";
+import About from './components/pages/About.js';
+import Skills from './components/pages/Skills.js';
+import Experience from './components/pages/Experience.js';
+import Education from './components/pages/Education.js';
+import Portfolio from './components/pages/Portfolio.js';
+import Contact from './components/pages/Contact.js';
 //pages
-import Pdf1 from "./components/pages/Pdf1.js";
-import NoMatch from "./components/pages/Error404";
+import Pdf1 from './components/pages/Pdf1.js';
+import NoMatch from './components/pages/Error404';
 
 const AppRouter = () => {
-
-    return (
-
-        <Switch>
-<Route path="/"><About /> </Route>
-<Route path="/skills"><Skills /> </Route>
-<Route path="/experience"><Experience /> </Route>
-<Route path="/education"><Education /> </Route>
-<Route path="/portfolio"><Portfolio /> </Route>
-<Route path="/contact"><Contact /> </Route>
-<Route path="/resume"><Pdf1 /> </Route>
-<Route><NoMatch /></Route>
-         </Switch>
+  return (
+    <Switch>
+      <Route exact path="/"><About /> </Route>
+      <Route path="/skills"><Skills /> </Route>
+      <Route path="/experience"><Experience /> </Route>
+      <Route path="/education"><Education /> </Route>
+      <Route path="/portfolio"><Portfolio /> </Route>
+      <Route path="/contact"><Contact /> </Route>
+      <Route path="/resume"><Pdf1 /> </Route>
+      <Route><NoMatch /></Route>
+    </Switch>
   );
 };
 
